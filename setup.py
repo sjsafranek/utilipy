6074761bfd2c37c5ebcf56acf123a0b97c17726d
Stefan Safranek <stefan@stefan>
2018-07-18 09:55:38 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='utilipy',
+      version='1.0',
+      description='Utility functions for Python3',
+      author='Stefan Safranek',
+      author_email='sjsafranek@gmail.com',
+      url='https://github.com/sjsafranek/utilipy',
+      packages=['utilipy'],
+     )
